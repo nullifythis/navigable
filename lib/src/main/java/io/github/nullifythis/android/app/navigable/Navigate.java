4ad9c5e792af5a92ee26da854edde2cfe1062bc2
Jonathan Cobb <jon.cobber@gmail.com>
2018-10-17 00:21:38 -0400

    Android Build Update
    Gradle -> 4.6
    AS Gradle -> 3.2.1
    SDK -> 28
    Min SDK -> 19
    Android X

--- lib/src/main/java/io/github/nullifythis/android/app/navigable/Navigate.java
+++ lib/src/main/java/io/github/nullifythis/android/app/navigable/Navigate.java
@@ -1,7 +1,7 @@
 package io.github.nullifythis.android.app.navigable;
 
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 public class Navigate {
 
